c540703420ac66167f8bd0df61b0ee2788c8fa3b
Fixolito <47037632+Fixolito@users.noreply.github.com>
2024-02-08 14:37:54 +0100

    added testing

---
+++ DualNumbers.Test/TDualMethods.cs
@@ -0,0 +1,51 @@
+namespace DualNumbers.Test;
+
+public class TDualMethods
+{
+    [TestFixture]
+    public class DualTests
+    {
+        [TestCase(1.0, 2.0, 3.0, 4.0, 4.0, 6.0)]
+        [TestCase(-1.0, 2.5, 3.1, -4.0, 2.1, -1.5)]
+        [TestCase(0.0, 0.0, 0.0, 0.0, 0.0, 0.0)]
+        public void Add_TwoDuals_ReturnsCorrectDual(double leftReal, double leftDual, double rightReal, double rightDual, double expectedReal, double expectedDual)
+        {
+            var left = new Dual(leftReal, leftDual);
+            var right = new Dual(rightReal, rightDual);
+            var expected = new Dual(expectedReal, expectedDual);
+
+            var result = Dual.Add(left, right);
+
+            Assert.That(result.real, Is.EqualTo(expected.real));
+            Assert.That(result.dual, Is.EqualTo(expected.dual));
+        }
+
+        [TestCase(1.0, 2.0, 3.0, 4.0, 2.0)]
+        [TestCase(-1.0, 2.5, 3.5, 2.5, 2.5)]
+        [TestCase(0.0, 0.0, 1.0, 1.0, 0.0)]
+        public void Add_DualAndDouble_ReturnsCorrectDual(double leftReal, double leftDual, double right, double expectedReal, double expectedDual)
+        {
+            var left = new Dual(leftReal, leftDual);
+            var expected = new Dual(expectedReal, expectedDual);
+
+            var result = Dual.Add(left, right);
+
+            Assert.That(result.real, Is.EqualTo(expected.real));
+            Assert.That(result.dual, Is.EqualTo(expected.dual));
+        }
+
+        [TestCase(3.0, 1.0, 2.0, 4.0, 2.0)]
+        [TestCase(3.5, -1.0, 2.5, 2.5, 2.5)]
+        [TestCase(1.0, 0.0, 0.0, 1.0, 0.0)]
+        public void Add_DoubleAndDual_ReturnsCorrectDual(double left, double rightReal, double rightDual, double expectedReal, double expectedDual)
+        {
+            var right = new Dual(rightReal, rightDual);
+            var expected = new Dual(expectedReal, expectedDual);
+
+            var result = Dual.Add(left, right);
+
+            Assert.That(result.real, Is.EqualTo(expected.real));
+            Assert.That(result.dual, Is.EqualTo(expected.dual));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+![.NET](https://github.com/Fixolito/DualNumbers/workflows/.NET/badge.svg)
+
+[![Coverage Status](https://coveralls.io/repos/github/Fixolito/DualNumbers/badge.svg?branch=main)](https://coveralls.io/github/Fixolito/DualNumbers?branch=main)
+
 # DualNumbers
 C# library for dual numbers.
 If ``Complex`` has implemented it, this implementation should have it too.
