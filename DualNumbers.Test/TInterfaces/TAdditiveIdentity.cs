35d03aac58ced52bc324080ddac0ed44d3ce1139
n1k1010 <105913072+n1k1010@users.noreply.github.com>
2024-06-26 14:33:22 +0200

    + T MultiplicativeIdentity + changes on TAdditiveIdentity

--- DualNumbers.Test/TInterfaces/TAdditiveIdentity.cs
+++ DualNumbers.Test/TInterfaces/TAdditiveIdentity.cs
@@ -5,7 +5,7 @@ using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DualNumbers.Test.TInterfaces
+namespace DualNumbers.Test
 {
     [TestFixture]
     public class DualAdditiveIdentityTests
